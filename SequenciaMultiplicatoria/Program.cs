b55aa0432787bfbaccd88392db58efe84b998af7
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-04-27 08:51:52 -0300

    Sequencia

---
+++ SequenciaMultiplicatoria/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Digite um valor numero inteiro para ser sequenciado:");
+int sequencia = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i <= sequencia; i++)
+{
+    Console.WriteLine($"{i} x {sequencia} = {i*sequencia}");
+}
