5338b8aa747b68e167ad5d0c2c44504c82996708
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-05-01 10:25:46 -0300

    Bat para bkp d file toda vez em nova pasta

---
+++ CopiarAula.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem Obter data atual
+set "data=!date:~-4,4!!date:~-10,2!!date:~7,2!"
+
+rem Obter hora atual
+set "hora=!time:~0,2!!time:~3,2!!time:~6,2!"
+
+rem Criar diretório com data e hora
+mkdir "!data!!hora!"
+
+rem Navegar para o diretório criado
+cd "!data!!hora!"
+
+copy C:\Users\imlbl\OneDrive\Desktop\GitRepos\Curso-C#\AcompanharVidAula\AcompanharVidAula\Program.cs
+
+endlocal
+
