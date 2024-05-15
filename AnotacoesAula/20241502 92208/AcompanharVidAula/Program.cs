29e474aa833c1bfd7a3261589f08406cb0ba809c
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-05-15 09:52:14 -0300

    aula

---
+++ AnotacoesAula/20241502 92208/AcompanharVidAula/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace Course
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			char genero = 'F';
+			int idade = 32;
+			double saldo = 10.35784;
+			string nome = "Maria";
+
+            Console.Write("Bom dia!");
+            Console.WriteLine("Boa tarde");
+            Console.WriteLine("Boa noite");
+			Console.WriteLine("------------------------------------");
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo);
+            Console.WriteLine(nome);
+			//F2 duas casas decimas F3 3 casas e assim por diante
+            Console.WriteLine(saldo.ToString("F2"));
+            Console.WriteLine(saldo.ToString("F4"));
+			//trocar virgula por ponto
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
+        }
+	}
+}
