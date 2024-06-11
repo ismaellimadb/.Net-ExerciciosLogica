de9e221a62f5f2aebb7ec7adfe749b236a607fd2
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-06-11 11:01:25 -0300

    conversao

---
+++ AnotacoesAula/20241102110115/AcompanharVidAula/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace Course
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double a;
+			int b;
+			//valores apos ponto/virgula truncados na conversao explicita pra int
+			a = 5.1;
+			b = (int)a;
+            Console.WriteLine(b);
+
+        }
+	}
+}
