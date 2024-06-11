fc14e9286f6b08c1193444dca0d8313993afc750
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-06-11 10:41:52 -0300

    conversao

---
+++ AnotacoesAula/20241102104101/AcompanharVidAula/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Course
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//conversao implicita pq float cabe no double
+			float x = 4.5f;
+			double y = x;
+
+            Console.WriteLine(y);
+
+			//conversao explicita pq float não cabe no double
+			double a = 5.1f;
+			float b = (float)a;
+
+			Console.WriteLine(a);
+		}
+	}
+}
