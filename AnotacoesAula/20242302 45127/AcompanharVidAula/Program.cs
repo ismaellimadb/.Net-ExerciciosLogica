4c943315f2fe10e425be166cb8e615154ddd09c9
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-07-23 05:10:36 -0300

    commit

---
+++ AnotacoesAula/20242302 45127/AcompanharVidAula/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace Course
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = 5;
+			int b = 2;
+
+			double resultado = (double) a / b;
+
+            Console.WriteLine(resultado);
+
+        }
+	}
+}
