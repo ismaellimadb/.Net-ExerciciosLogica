679125ddaeea4e185b4951a6bee6b51b51a6758e
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-07-24 12:27:19 -0300

    Ex1

---
+++ AnotacoesAula/20242402122645/AcompanharVidAula/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+  Exercício 01
+	Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+	mensagem explicativa, conforme exemplo
+Exemplo
+Entrada:
+10
+Saida
+20
+
+ */
+
+using System;
+using System.Globalization;
+
+namespace Course
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Entrada: ");
+			int a = int.Parse(Console.ReadLine());
+			int b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Saida: \nSOMA = {a + b}");
+        }
+	}
+}
