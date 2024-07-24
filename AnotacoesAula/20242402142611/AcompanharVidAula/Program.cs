102dfe9b9b0ba6a1f3dea63e500358eb9eb6492a
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-07-24 14:26:34 -0300

    Ex5

---
+++ AnotacoesAula/20242402142611/AcompanharVidAula/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+  Exercício 05
+	Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o
+código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago.
+ */
+
+using System;
+using System.Globalization;
+
+namespace Course
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Entrada: ");
+			string[] vet = Console.ReadLine().Split(' ');
+			int a = int.Parse(vet[0]);
+			int b = int.Parse(vet[1]);
+			double c = double.Parse(vet[2], CultureInfo.InvariantCulture); ;
+
+			string[] vet2 = Console.ReadLine().Split(' ');
+			int d = int.Parse(vet2[0]);
+			int e = int.Parse(vet2[1]);
+			double f = double.Parse(vet2[2], CultureInfo.InvariantCulture); ;
+
+			double resultado = (b * c) + (e * f);
+
+			Console.WriteLine($"VALOR A PAGAR: R$ {resultado.ToString("F2",CultureInfo.InvariantCulture)}");
+		}
+	}
+}
