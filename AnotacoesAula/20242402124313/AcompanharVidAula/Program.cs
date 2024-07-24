08a111c98be7b51d841433c5370db492dda5723c
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-07-24 12:43:25 -0300

    Ex2

---
+++ AnotacoesAula/20242402124313/AcompanharVidAula/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+  Exercício 02
+	Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+casas decimais 
+ */
+
+using System;
+using System.Globalization;
+
+namespace Course
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Entrada: ");
+			double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+			double raio = Math.PI * Math.Pow(a, 2);
+			Console.WriteLine($"Saida:\nA = {raio.ToString("F4", CultureInfo.InvariantCulture)}");
+		}
+	}
+}
