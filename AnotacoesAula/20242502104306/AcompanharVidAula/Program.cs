734765e7c07a448f3f6c5a994a59eb14bbf673e0
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-07-25 10:43:30 -0300

    au27encadeamento

---
+++ AnotacoesAula/20242502104306/AcompanharVidAula/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+	static void Main()
+	{
+        Console.WriteLine("Qual a hora atual?");
+		int hora = int.Parse(Console.ReadLine());
+		
+		if (hora < 12)
+		{
+            Console.WriteLine("Bom dia");
+        }
+		else if (hora < 18)
+		{
+			Console.WriteLine("Boa tarde");
+		}
+		else
+		{
+			Console.WriteLine("Boa noite!");
+		}
+		//com ekse if para de testar o programa, com else if ja para de testar alem de ser mais seguro com else if pois um if so tem mais chande de erro.
+		//if (hora >= 12 && hora < 18)
+		//{
+		//          Console.WriteLine("Boa tarde");
+		//      }
+		//if (hora >= 18)
+		//{
+		//	Console.WriteLine("Boa noite!");
+		//}
+	}
+}
