9471b52c61b9592e6cd9592535156eddd2309eb2
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-07-25 09:31:17 -0300

    au26PrecendenciaOperadorLogico

---
+++ AnotacoesAula/20242502 93049/AcompanharVidAula/Program.cs
@@ -0,0 +1,29 @@
+﻿/* 
+Precedencia: ! > && > ||
+2 > 3 || 4 != 5 = true
+!(2>3) && 4 != 5 = true
+*/
+using System;
+class Program
+{
+	static void Main()
+	{
+		bool c1 = 2 > 3 || 4 != 5; //true
+
+		bool c2 = !(2 > 3) && 4 != 5;//true
+
+
+		Console.WriteLine(c1);
+		Console.WriteLine(c2);
+
+		Console.WriteLine("---------------");
+
+		bool c3 = 10 < 5; // false
+		
+		bool c4 = c1 || c2 && c3; //true
+
+        Console.WriteLine(c3);
+        Console.WriteLine(c4);
+
+    }
+}
