baf64f4bdb51be00ad891322bf36b1d7c2bbdc87
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-07-25 11:58:06 -0300

    au28escopoinit

---
+++ AnotacoesAula/20242502115746/AcompanharVidAula/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+	static void Main()
+	{
+		double preco = double.Parse(Console.ReadLine());
+		double desconto = 0.0;
+
+		if (preco > 100)
+		{
+			desconto = preco * 0.1;
+		}
+        Console.WriteLine(desconto);
+    }
+}
