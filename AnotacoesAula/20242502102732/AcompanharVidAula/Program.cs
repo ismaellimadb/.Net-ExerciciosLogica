110d082c8f928ad19b9f84ac5c7c5cf41a162743
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-07-25 10:27:52 -0300

    au27condisimp1

---
+++ AnotacoesAula/20242502102732/AcompanharVidAula/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+	static void Main()
+	{
+		int x = 10;
+
+        Console.WriteLine("Bom dia");
+		
+		if (x > 5)
+		{
+			Console.WriteLine("Boa tarde");
+		}
+		
+		
+		Console.WriteLine("Boa noite");
+
+    }
+}
