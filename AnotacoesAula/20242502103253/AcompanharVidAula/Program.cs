80d5690e68e245d2a7c9eec4518e14a0ef4c2c30
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-07-25 10:33:17 -0300

    au27ParOuImpar

---
+++ AnotacoesAula/20242502103253/AcompanharVidAula/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+	static void Main()
+	{
+        Console.WriteLine("Entre com um número inteiro:");
+		int x = int.Parse(Console.ReadLine());
+		
+		if (x % 2 == 0) //resto da divisao de 2 = 0 verficando par
+		{
+			Console.WriteLine("Par!");
+		}
+		else
+		{
+            Console.WriteLine("Impar!");
+        }
+    }
+}
