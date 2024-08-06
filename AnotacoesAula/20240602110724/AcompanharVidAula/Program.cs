591f3ff80a5589e86e7e67af14d0cc1509654f21
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-08-06 11:09:15 -0300

    Ex1 if else

---
+++ AnotacoesAula/20240602110724/AcompanharVidAula/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ Ex 1
+ Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não
+ */
+
+using System;
+class Program
+{
+	static void Main()
+	{
+		int inteiro = int.Parse(Console.ReadLine());
+
+		if (inteiro > 0)
+		{
+			Console.WriteLine("NAO NEGATIVO");
+		}
+		else
+		{
+			Console.WriteLine("NEGATIVO");
+		}
+        
+    }
+}
