a73bfaad36432cd02800002cc2968000a37bb017
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-08-06 11:22:32 -0300

    Ex2 if else

---
+++ AnotacoesAula/20240602112222/AcompanharVidAula/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ Ex 2
+ Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+*/
+
+using System;
+class Program
+{
+	static void Main()
+	{
+		int inteiro = int.Parse(Console.ReadLine());
+
+		if (inteiro % 2 == 0)
+		{
+			Console.WriteLine("PAR");
+		}
+		else
+		{
+			Console.WriteLine("IMPAR");
+		}
+        
+    }
+}
