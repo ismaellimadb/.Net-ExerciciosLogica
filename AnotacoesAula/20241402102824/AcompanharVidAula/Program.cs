5626ea831e82f31af339cfcffb52ce04925d82d5
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-08-14 10:29:30 -0300

    Ex 6 intervalo

---
+++ AnotacoesAula/20241402102824/AcompanharVidAula/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ Ex 6
+Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos
+seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em
+nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”
+*/
+
+using System;
+using System.Globalization;
+class Program
+{
+	static void Main()
+	{
+		double entrada = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+		if(entrada >= 0 && entrada <= 25)
+		{
+			Console.WriteLine("Intervalo: 0,25");
+		}
+		else if (entrada >= 25 && entrada <= 50)
+		{
+			Console.WriteLine("Intervalo: 25,50");
+		}
+		else if (entrada >= 50 && entrada <= 75)
+		{
+			Console.WriteLine("Intervalo: 50,75");
+		}
+		else if (entrada >= 75 && entrada <= 100)
+		{
+			Console.WriteLine("Intervalo: 75,100");
+		}
+		else
+		{
+			Console.WriteLine("Fora de intervalo");
+		}
+	}
+}
