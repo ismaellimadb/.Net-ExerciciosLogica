e6df8a1d516f419bcf90e88460e25bcf4015d26b
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-10-15 16:42:39 -0300

    whilecombustivel

---
+++ AnotacoesAula/20241502164223/AcompanharVidAula/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int combustivel = int.Parse(Console.ReadLine());
+			int alcool = 0;
+			int gasolina = 0;
+			int diesel = 0;
+			while (combustivel != 4)
+			{
+				if (combustivel == 1)
+				{
+					alcool++;
+				}
+				else if (combustivel == 2)
+				{
+					gasolina++;
+				}
+				else if (combustivel == 3)
+				{
+					diesel++;
+				}
+				combustivel = int.Parse(Console.ReadLine());
+			}
+            Console.WriteLine($"MUITO OBRIGADO\nAlcool: {alcool}\nGasolina: {gasolina}\nDiesel: {diesel}");
+        }
+	}
+}
