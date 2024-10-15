84890072d6bc4de7a1d4a379f517476b4fcdf9f7
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-10-15 16:20:20 -0300

    senhawhile

---
+++ AnotacoesAula/20241502162002/AcompanharVidAula/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Digite a senha: ");
+			string x = Console.ReadLine();
+
+			while (x != "2002")
+			{
+				Console.WriteLine("Senha Invalida");
+				x = Console.ReadLine();
+			}
+
+            Console.WriteLine("Acesso Permitido");
+        }
+
+	}
+}
