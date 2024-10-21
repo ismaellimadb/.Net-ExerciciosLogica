568a96915c9adf61930ee01b24ecb3102bb506ec
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-10-21 13:12:14 -0300

    Ex6forloopDivRest

---
+++ AnotacoesAula/20242102131145/AcompanharVidAula/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Ex6
+Ler um número inteiro N e calcular todos os seus divisores.
+*/
+
+using System;
+using System.Globalization;
+
+namespace Course
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			for (int i = 1; i <= n; i++)
+			{
+				if (n % i == 0)
+				{
+					Console.WriteLine(i);
+				}
+			}
+		}
+	}
+}
