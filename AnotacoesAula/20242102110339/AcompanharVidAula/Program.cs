24349f8691c4852e39c2f7557b2e5b39833c6105
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-10-21 11:04:05 -0300

    Ex1forloopImpar

---
+++ AnotacoesAula/20242102110339/AcompanharVidAula/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Ex1
+Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
+X, se for o caso.
+*/
+
+using System;
+using System.Globalization;
+
+namespace Course
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.Write("Digite o valor: ");
+			int x = int.Parse(Console.ReadLine());
+
+			for (int i = 1;  i <= x; i++)
+			{
+				if (i % 2 != 0)
+				{
+					Console.WriteLine("Valor Impar #{0}: ", i);
+				}
+			}
+        }
+	}
+}
