ca1a5592ac6b4e722b5b6618277eaf929cdd3aaf
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2024-10-22 11:06:04 -0300

    Ex1POOPessoaIdade

---
+++ AnotacoesAula/20242202110527/AcompanharVidAula/Program.cs
@@ -0,0 +1,36 @@
+﻿using AcompanharVidAula;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pessoa pessoa1, pessoa2;
+
+			pessoa1 = new Pessoa();
+			pessoa2 = new Pessoa();
+
+			pessoa1.Nome = Console.ReadLine();
+			pessoa1.Idade = int.Parse(Console.ReadLine());
+
+			pessoa2.Nome = Console.ReadLine();
+			pessoa2.Idade = int.Parse(Console.ReadLine());
+			
+			if (pessoa1.Idade > pessoa2.Idade)
+			{
+				Console.WriteLine("{0} é mais velho que {1}", pessoa1.Nome, pessoa2.Nome);
+			}
+			else if (pessoa1.Idade == pessoa2.Idade)
+			{
+                Console.WriteLine("Ambos tem a mesma idade.");
+            }
+			else
+			{
+				Console.WriteLine("{1} é mais velho que {0}", pessoa1.Nome, pessoa2.Nome);
+			}
+		}
+	}
+}
