329a5b0d15f5457e0259e5d1d504faf14afd5cf0
Ismael de Carvalho Lima <42699465+ismaellimadb@users.noreply.github.com>
2025-02-20 15:26:37 -0300

    Ex2 SalarioFuncionario

---
+++ AnotacoesAula/20252002152539/AcompanharVidAula/Program.cs
@@ -0,0 +1,46 @@
+﻿using AcompanharVidAula;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string nome;
+			double salarioBruto, salarioLiquido, porcentagem, imposto, aumento;
+
+			Console.Write("Nome: ");
+			nome = Console.ReadLine();
+
+			Console.Write("Salário bruto: ");
+			salarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+			Console.Write("Imposto: ");
+			imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+			salarioLiquido = SalarioLiquido(salarioBruto, imposto);
+
+			Console.Write($"\nFuncionario: {nome}, $ {salarioLiquido}");
+
+			Console.WriteLine("\nDigite a porcentagem para aumentar o salário: ");
+			porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+			aumento = AumentarSalario(porcentagem, salarioBruto);
+
+			Console.Write($"\nDados atualizados: {nome}, $ {salarioLiquido + aumento}");
+		}
+
+		public static double SalarioLiquido(double salarioBruto, double imposto)
+		{
+			return salarioBruto - imposto;
+		}
+
+		public static double AumentarSalario(double porcentagem, double salarioBruto)
+		{
+			//return salarioBruto * double.Parse($"0.{porcentagem}", CultureInfo.InvariantCulture);
+			return salarioBruto * (porcentagem / 100.0);
+		}
+	}
+}
---
+++ ExerciciosUML/Retangulo.cs
@@ -0,0 +1,43 @@
+/*Retangulo
+- Largura : double
+- Altura : double
++ Area() : double
++ Perimetro() : double
++ Diagonal() : double
+*/
+
+using System;
+using System.Globalization;
+
+namespace Course {
+    class Retangulo {
+        public double Largura;
+        public double Altura;
+
+        public double Area() {
+            return Largura * Altura;
+        }
+
+        public double Perimetro() {
+            return 2 * (Largura + Altura);
+        }
+
+        public double Diagonal() {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA = " + r.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + r.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + r.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
